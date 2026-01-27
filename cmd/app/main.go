66ff77eb73244ec8a42bcc6a66ff55cfd8d421ad
perezvonish <shokilak@gmail.com>
2026-01-28 01:08:44 +0500

    feat: update main.go with http-server bootstrap + graceful shutdown

---
+++ cmd/app/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"perezvonish/plata-test-assignment/internal/adapters/incoming/rest"
+	"perezvonish/plata-test-assignment/internal/shared/config"
+	"syscall"
+	"time"
+)
+
+func main() {
+	cfg, err := config.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	httpServer := rest.NewServer(ctx, *cfg)
+
+	go httpServer.Start()
+
+	<-ctx.Done()
+	fmt.Println("\nShutdown signal received...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := httpServer.Stop(shutdownCtx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	fmt.Println("Application stopped gracefully")
+}
